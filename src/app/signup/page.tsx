d1b8416d049a293550a214b028b645f56f6a0291
Xantrail Studio Official <xantrailstudio@gmail.com>
2026-04-27 03:01:44 -0700

    Completing the Login Page front-end

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,25 +13,32 @@ export default function Signup() {
         className="background-img"
       />
       <div className="justify-center align-center width-full height-full flex background-transparent color-white">
-        <div className="signup-form background-transparent background-blur flex flex-column padding-25">
+        <div className="signup-form background-transparent background-blur-25 flex flex-column padding-25">
           <div className="form-title text-center flex align-center justifyenter">
             <span className="form-title-side-decoration-white"></span>
             <h1>Signup to Cloud Todo</h1>
             <span className="form-title-side-decoration-white"></span>
           </div>
           <div className="form flex flex-column">
-            <div className="email-input-box">
+            <div className="email-input-box flex flex-column margin-top-12">
               <label htmlFor="email">User Email</label>
-              <input type="text" id="email" className="input input-text" />
+              <input
+                type="text"
+                id="email"
+                className="input input-text border-1-solid-white"
+              />
             </div>
-            <div className="password-input-box">
+            <div className="password-input-box flex flex-column margin-top-12">
               <label htmlFor="password">User Password</label>
               <input
                 type="text"
                 id="password"
-                className="input input-password"
+                className="input input-password border-1-solid-white"
               />
             </div>
+            <button className="btn btn-transparent btn-transparent-white margin-top-12">
+              Submit
+            </button>
           </div>
         </div>
       </div>
